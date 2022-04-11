e173cbc23cbd75945e515506a4bf334259f24197
Ryan Hueckel <77073352+jrhuecke@users.noreply.github.com>
2022-04-10 22:43:47 -0700

    fixed restart and added return to menu functionality (and finished game)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -61,8 +61,8 @@ class Play extends Phaser.Scene {
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
-            this.GameOver = true;
+            this.add.text(game.config.width/2, game.config.height/2 + 64, '(R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
         }, null, this);
     }
 
@@ -70,6 +70,9 @@ class Play extends Phaser.Scene {
         // check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
         }
         this.starfield.tilePositionX -= 4;
         if (!this.gameOver) {               
