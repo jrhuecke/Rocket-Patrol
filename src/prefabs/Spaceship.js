ad4ca1bee7a122f07c066964a8adb167208ef3de
Ryan Hueckel <77073352+jrhuecke@users.noreply.github.com>
2022-04-10 22:35:01 -0700

    finished main gameplay loop

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,8 +1,21 @@
-class Spaceship extends Phaser.GameObject.Sprite {
+class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.points = pointValue;
-        this.moveSpeed = 3;
+        this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update() {
+        //move spaceship left
+        this.x -= this.moveSpeed;
+        //wrap around from left to right
+        if (this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
