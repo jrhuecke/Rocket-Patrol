406e912cbf282b8dd4326f73acfe4e65e47afc5b
Ryan Hueckel <77073352+jrhuecke@users.noreply.github.com>
2022-04-10 20:31:22 -0700

    Did rocket class

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,8 @@
+class Spaceship extends Phaser.GameObject.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = 3;
+    }
+}
