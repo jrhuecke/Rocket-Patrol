e8bc203095ae5a56e300698563246d12406f9efe
Ryan Hueckel <jrhuecke@ucsc.edu>
2022-04-14 20:07:20 -0700

    fixed

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,7 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         //add object to existing scene
         scene.add.existing(this);
         this.isFiring = false;
-        this.moveSpeed = 3;
+        this.moveSpeed = 2;
         this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
